f9e3286fe6aef2d5d7fbe1a8f3e4bcf75ff4ffe9
Link <1304124478@qq.com>
2024-01-05 23:19:21 +0800

    删除文件

--- BmpManager/custom/qgraphicsitembase.cpp
+++
@@ -1,6 +0,0 @@
-#include "qgraphicsitembase.h"
-
-QVector<QGraphicsItemBase::AuxiliaryLine> QGraphicsItemBase::auxiliaryLines;
-
-bool QGraphicsItemBase::AuxiliaryLine::lock;
-bool QGraphicsItemBase::AuxiliaryLine::hide;
